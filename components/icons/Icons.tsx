9a65f1042c7d6e51f6ddffd09999d974ca726149
Tobias Brendler <tbrendler@gmail.com>
2025-09-15 22:34:43 +0200

    Downloadicon changed

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -115,6 +115,6 @@ export const SettingsIcon: React.FC = () => (
 
 export const InstallIcon: React.FC = () => (
   <IconWrapper>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
   </IconWrapper>
 );
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Dynamic cache name with timestamp to force updates
-const CACHE_VERSION = 1757957219420; // This will be replaced by build process
+const CACHE_VERSION = 1757968362401; // This will be replaced by build process
 const CACHE_NAME = `markdown-editor-pro-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
